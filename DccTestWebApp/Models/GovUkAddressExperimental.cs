e557a8132eb0282293446155a06aaa9aae3023ba
Dan Corder <dan.corder@softwire.com>
2020-01-27 16:41:48 +0000

    refactor: tidy up

--- DccTestWebApp/Models/GovUkAddressExperimental.cs
+++ DccTestWebApp/Models/GovUkAddressExperimental.cs
@@ -1,13 +1,7 @@
 ﻿using GovUkDesignSystem;
 using GovUkDesignSystem.Attributes;
-using GovUkDesignSystem.Attributes.ValidationAttributes;
 using GovUkDesignSystem.ModelBinders;
-using GovUkDesignSystem.Validators;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DccTestWebApp.Models
 {
@@ -16,7 +10,6 @@ namespace DccTestWebApp.Models
         [ModelBinder(typeof(GovUkMandatoryIntBinder))]
         [GovUkDisplayNameForErrors(NameAtStartOfSentence = "Test mandatory int", NameWithinSentence = "test mandatory int")]
         public int MandatoryInt { get; set; }
-        
         public int? ID { get; set; }
         public string Street { get; set; }
         public string PostCode { get; set; }
