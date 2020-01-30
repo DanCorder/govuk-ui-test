042d281d83997c2e4eaa12fb95ee46c168d4365a
Dan Corder <dan.corder@ofgem.gov.uk>
2020-01-30 12:07:41 +0000

    feat: New and old radio buttons both working

--- DccTestWebApp/Models/GovUkAddressExperimental.cs
+++ DccTestWebApp/Models/GovUkAddressExperimental.cs
@@ -8,6 +8,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DccTestWebApp.Models
 {
+    public enum RadioButtonValues
+    {
+        [GovUkRadioCheckboxLabelText(Text = "Value 1")]
+        radioValue1,
+
+        [GovUkRadioCheckboxLabelText(Text = "Value 2")]
+        radioValue2,
+
+        [GovUkRadioCheckboxLabelText(Text = "Value 3")]
+        radioValue3
+    }
+
     public class GovUkAddressExperimental : GovUkViewModel //qq:DCC remove inheritance
     {
         [ModelBinder(typeof(GovUkMandatoryIntBinder))]
@@ -21,6 +33,17 @@ namespace DccTestWebApp.Models
         [ModelBinder(typeof(GovUkMandatoryStringBinder))]
         [GovUkDataBindingStringErrorText(ErrorMessageIfMissing = "Enter a binder validated text area string")]
         public string BinderValidatedTextArea { get; set; }
+
+        //[ModelBinder(typeof(GovUkMandatoryRadioBinder))]
+        [Required(ErrorMessage = "Select a binder validated radio option")]
+        public RadioButtonValues? BinderValidatedRadio { get; set; }
+
+        [GovUkValidateRequired(ErrorMessageIfMissing = "Select a custom validated radio option")]
+        [GovUkDisplayNameForErrors(
+            NameAtStartOfSentence = "Custom validated radio option",
+            NameWithinSentence = "Custom validated radio option"
+        )]
+        public RadioButtonValues? CustomValidatedRadio { get; set; }
 
         [Required]
         public DccAddressChild Child { get; set; } = new DccAddressChild();
@@ -39,5 +62,9 @@ namespace DccTestWebApp.Models
         [ModelBinder(typeof(GovUkMandatoryStringBinder))]
         [GovUkDataBindingStringErrorText(ErrorMessageIfMissing = "Enter a child text area string")]
         public string ChildTextArea { get; set; }
+
+        //[ModelBinder(typeof(GovUkMandatoryRadioBinder))]
+        [Required(ErrorMessage = "Select a child radio option")]
+        public RadioButtonValues? ChildValidatedRadio { get; set; }
     }
 }
